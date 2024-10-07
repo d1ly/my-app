695ef0d9ce4349ed8c32debebcc4d08556a1e864
d1ly <d1ly@ucsd.edu>
2024-10-07 15:22:12 -0700

    Exercise up to 4.2.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import './App.css';
 import { Label, Note } from "./types"; // Import the Label type from the appropriate module
 import { dummyNotesList } from "./constants"; // Import the dummyNotesList from the appropriate module
-import "./hooksExercise";
+import ClickCounter from "./hooksExercise";
 
 
 function App() {
   
  return (
    <div className='app-container'>
+    
       <form className="note-form">
             <div><input placeholder="Note Title"></input></div>
 
             <div><textarea></textarea></div>
 
             <div><button type="submit">Create Note</button></div>
+            
       </form>
+      
+
       <div className="notes-grid">
+      <ClickCounter />
        {dummyNotesList.map((note) => (
          <div
            key={note.id}
