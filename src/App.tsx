8d7c385d09d7f733890f60d2e2c66775c87a315d
d1ly <d1ly@ucsd.edu>
2024-10-13 19:02:58 -0700

    4.3.3 done

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,30 @@ import { ThemeContext, themes } from "./themeContext";
 
 function App() {
 
-  const [notes, setNotes] = useState(dummyNotesList.map(note => ({ ...note, favorited: false }))); 
+  const [notes, setNotes] = useState(dummyNotesList); 
+  const initialNote = {
+    id: -1,
+    title: "",
+    content: "",
+    label: Label.other,
+    favorited: false,
+  };
+  const [createNote, setCreateNote] = useState(initialNote);
+
+  // functions for creating, updating, deleting notes
+  const createNoteHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("title: ", createNote.title);
+    console.log("content: ", createNote.content);
+    createNote.id = notes.length + 1;
+    setNotes([createNote, ...notes]);
+    setCreateNote(initialNote);
+  };
+
+  const updateNoteHandler = (id: number, updatedNote: Partial<Note>) => {
+    setNotes(notes.map(note => (note.id  === id ? { ...note, ...updatedNote } : note)));
+  };
+
   const [favoriteTitles, setFavoriteTitles] = useState<string[]>([]); 
   const [currentTheme, setCurrentTheme] = useState(themes.light);
 
@@ -39,17 +62,42 @@ function App() {
   <ThemeContext.Provider value={currentTheme}>
    <div className='app-container' style={{ background: currentTheme.background, color: currentTheme.foreground}}>
     
-      <form className="note-form">
-            <div><input placeholder="Note Title"></input></div>
 
-            <div><textarea></textarea></div>
+  	<form className="note-form" onSubmit={createNoteHandler}>
+    	<div>
+      	<input
+        	placeholder="Note Title"
+        	onChange={(event) =>
+          	setCreateNote({ ...createNote, title: event.target.value })}
+        	required>
+      	</input>
+    	</div>
 
-            <div><button type="submit">Create Note</button></div>
-            
-      </form>
+    	<div>
+      	<textarea
+        	onChange={(event) =>
+          	setCreateNote({ ...createNote, content: event.target.value })}
+        	required>
+      	</textarea>
+    	</div>
+
+  <div>
+     	<select
+       	onChange={(event) =>
+         	setCreateNote({ ...createNote, label: event.target.value as Label})}
+       	required>
+       	<option value={Label.personal}>Personal</option>
+       	<option value={Label.study}>Study</option>
+       	<option value={Label.work}>Work</option>
+       	<option value={Label.other}>Other</option>
+     	</select>
+   	</div>
+
+    	<div><button type="submit">Create Note</button></div>
+  	</form>
       
       <div className="notes-grid">
-       {dummyNotesList.map((note) => (
+       {notes.map((note) => (
          <div
            key={note.id}
            className="note-item" 
@@ -61,9 +109,20 @@ function App() {
             </button>
              <button>x</button>
            </div>
-           <h2> {note.title} </h2>
-           <p> {note.content} </p>
-           <p> {note.label} </p>
+           <h2 contentEditable onBlur={(event) =>
+                updateNoteHandler(note.id, { title: event.currentTarget.textContent as string})
+              }> {note.title} </h2>
+           <p contentEditable onBlur={(event) =>
+                updateNoteHandler(note.id, { content: event.currentTarget.textContent as string})
+              }> {note.content} </p>
+            <select value={note.label} onChange={(event) =>
+                updateNoteHandler(note.id, { label: event.target.value as Label })
+            } >
+              <option value="Personal">Personal</option>
+              <option value="Study">Study</option>
+              <option value="Work">Work</option>
+              <option value="Other">Other</option>
+            </select>
          </div>
        ))}
       </div>
@@ -83,11 +142,3 @@ function App() {
 }
 
 export default App;
-
-/*
-function ListItem(item: Note) {
-  return (
-      <li key={item.title}> name={item.title} /{item.title}</li>
-  )
-}
-*/
